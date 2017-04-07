ad8a420ca8ff6347520ef88b4cd0253127a7b885
lech.piechota <lech.piechota@ocadotechnology.com>
2017-04-07 13:26:05 +0200

    Initial commit

---
+++ src/main/java/com/lpi/ann/AnnApplication.java
@@ -0,0 +1,12 @@
+package com.lpi.ann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnApplication.class, args);
+	}
+}
